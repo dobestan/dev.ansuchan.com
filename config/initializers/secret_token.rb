915a889656529c58e41ad106e615313b679caf04
dobestan <dobestan@gmail.com>
2014-05-10 14:44:07 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevAnsuchanCom::Application.config.secret_key_base = 'a0bf4c04681d0a639c35b7892346d0fdb9d7bb866d51fe1a5d38eadbf435c702d249f1b26604a2b4f3e17bf99b5163cf1437ed598424c590413aacf86ffaee51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevAnsuchanCom::Application.config.session_store :cookie_store, key: '_dev_ansuchan_com_session'
