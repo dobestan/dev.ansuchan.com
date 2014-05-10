bbde7251fa065c3f4fab6505eb0aee0b0a251cdb
dobestan <dobestan@gmail.com>
2014-05-10 16:53:27 +0900

    complete edit

--- app/controllers/share_links_controller.rb
+++ app/controllers/share_links_controller.rb
@@ -32,6 +32,28 @@ class ShareLinksController < ApplicationController
     end
   end
 
+  def edit
+    @share_link = ShareLink.find(params[:id])
+  end
+
+  def update
+    @share_link = ShareLink.find(params[:id])
+
+    if share_link_params[:shorten_url].empty?
+      new_shorten_url = generate_short_string
+    else
+      new_shorten_url = share_link_params[:shorten_url]
+    end
+
+    if @share_link.update(shorten_url: new_shorten_url)
+      # Success to Update
+      redirect_to @share_link
+    else
+      # Failed to Update
+      render 'edit'
+    end
+  end
+
   private
     def share_link_params
       params.require(:share_link).permit(:original_url, :shorten_url)
--- app/models/share_link.rb
+++ app/models/share_link.rb
@@ -1,5 +1,6 @@
 class ShareLink < ActiveRecord::Base
   before_save { self.original_url = original_url.downcase }
+
   validates :original_url, presence: true,
                           uniqueness: true
   validates :shorten_url, presence: true,
